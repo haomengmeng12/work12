294dea58c7691bfa0f97b132a7a7ced0fc9317ab
haomengmeng <1794290935@qq.com>
2018-06-15 08:40:28 +0800

    xiangmu zhenghe

---
+++ laravel/app/Models/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: D
+ * Date: 2018/6/13 0013
+ * Time: 9:47
+ */
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+    //
+}
---
+++ laravel/resources/views/Admin/users/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin')
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+@endsection
+
+@section('main')
+  <div class="pd-20">
+    <table class="table table-border table-bordered table-hover table-bg table-sort">
+      <thead>
+      <tr class="text-c">
+        <th width="100">用户名</th>
+        {{--<th width="40">头像</th>--}}
+        <th width="150">邮箱</th>
+        <th width="130">加入时间</th>
+        {{--<th width="70">状态</th>--}}
+        <th width="100">操作</th>
+      </tr>
+      </thead>
+      <tbody>
+        @inject('userPresenter', 'App\Presenters\UserPresenter')
+        @foreach ($users as $user)
+          <tr class="text-c">
+            <td>
+              <u style="cursor:pointer" class="text-primary">
+                {{ $user->name }}
+              </u>
+            </td>
+            {{--<td><img src="{{ $userPresenter->getAvatarLink($user->avatar) }}"></td>--}}
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->created_at }}</td>
+            {{--<td class="user-status">--}}
+              {{--{!! $userPresenter->getStatusSpan($user->is_active) !!}--}}
+            {{--</td>--}}
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <div id="pageNav" class="pageNav">
+        {{ $users->links() }}
+    </div>
+  </div>
+@endsection
